170356b7618ae80c6b01985adf4e6d586a0b8013
lodoss <developer@iMac-lodoss.local>
2017-12-12 12:27:08 +0300

    Changed a initialization db

--- db.js
+++ db.js
@@ -4,28 +4,20 @@ const Sequelize = require('sequelize');
 const config = require('./config/config');
 
 let db = null;
-let sequelize = null;
 
 module.exports = () => {
   if (!db) {
-    sequelize = new Sequelize(
+    db = new Sequelize(
       config.db.database,
       config.db.username,
       config.db.password,
       config.db.params,
     );
 
-    db = {
-      sequelize,
-      Sequelize,
-      models: {},
-    };
-
     const dir = path.join(__dirname, 'models/scheme');
     fs.readdirSync(dir).forEach((file) => {
       const modelDir = path.join(dir, file);
-      const model = sequelize.import(modelDir);
-      db.models[model.name] = model;
+      db.import(modelDir);
     });
 
     Object.keys(db.models).forEach((modelName) => {
@@ -33,6 +25,8 @@ module.exports = () => {
         db.models[modelName].associate(db.models);
       }
     });
+
+    db.sync();
   }
 
   return db;
