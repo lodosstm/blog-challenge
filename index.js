b8b8a385b29b597553ad93213217c8dd90507202
lodoss <developer@iMac-lodoss.local>
2017-12-12 17:51:58 +0300

    Changed the index file

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config/config');
+require('./db')();
+
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
-const port = 8080;
-app.listen(port, () => {
-  console.log(port);
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+
+app.use('/api', authRoutes);
+
+app.use('/', (req, res, next) => {
+  res.json(res.data);
+  next();
+});
+
+app.use('/', (err, req, res, next) => {
+  res.json(err);
+  next();
+});
+
+app.listen(config.general.port, () => {
+  console.log(config.general.port);
 });
