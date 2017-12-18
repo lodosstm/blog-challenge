31f64a10f6651e1f2804e3a9eb87f31f654a0a05
lodoss <developer@iMac-lodoss.local>
2017-12-18 14:57:45 +0300

    Adding changes in registration, adding users confirm

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,42 @@
-const User = require('../models/user');
+const users = require('../models/user');
+const redisClient = require('../common/redis');
+const config = require('../config/config');
+const randomstring = require('randomstring');
 
 const registration = async (req, res, next) => {
-  const user = await User.addNewUser(req.body);
-  res.data = {
-    email: user.email,
-    createdAt: user.created_at,
-  };
+  const { id } = await users.addNewUser(req.body);
+
+  const hashCode = randomstring.generate();
+
+  await redisClient.set(hashCode, id);
+  await redisClient.expire(hashCode, 24 * 60 * 60);
+
+  const link = `http://${config.general.host}:${config.general.port}/api/auth/confirm/${hashCode}`;
+  console.log(link);
+  res.data = { success: true };
+
   next();
+};
+
+const confirmUser = async (req, res, next) => {
+  const { params: { hash_code: hashCode } } = req;
+  const id = await redisClient.getAsync(hashCode);
+
+  if (!id) {
+    return Promise.reject({ success: false });
+  }
+
+  const affectedCount = await users.confirmUser(id);
+
+  if (affectedCount[0] > 0) {
+    res.data = { success: true };
+    return next();
+  }
+
+  return Promise.reject({ success: false });
 };
 
 module.exports = {
   registration,
+  confirmUser,
 };
