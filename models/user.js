bdf09f87c81e11fdf78b23eac0f1b394b7bbdf61
lodoss <developer@iMac-lodoss.local>
2017-12-18 14:09:35 +0300

    Adding changes in users model

--- models/user.js
+++ models/user.js
@@ -1,13 +1,37 @@
-const db = require('../db')();
+const users = require('../db')().models.user;
+const bcrypt = require('bcrypt-nodejs');
 
-const addNewUser = userdata =>
-  db.models.user.create({
+const hashingPassword = password =>
+  bcrypt.hashSync(password);
+
+const addNewUser = async (userdata) => {
+  const user = await users.create({
     first_name: userdata.first_name,
     last_name: userdata.last_name,
-    password: userdata.password,
+    password: hashingPassword(userdata.password),
     email: userdata.email,
   });
+  return user.toJSON();
+};
+
+const confirmUser = id =>
+  users.update({
+    status: true,
+  }, {
+    where: {
+      id,
+    },
+  });
+
+const findUserByEmail = email =>
+  users.findOne({ where: { email } });
+
+const findUserById = id =>
+  users.findById(id);
 
 module.exports = {
   addNewUser,
+  confirmUser,
+  findUserByEmail,
+  findUserById,
 };
