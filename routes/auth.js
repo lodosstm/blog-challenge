5d1a1b780d0c27fbe980018dfd120875b007e2ad
lodoss <developer@iMac-lodoss.local>
2017-12-18 13:00:13 +0300

    Adding a routes for registration, users confirm, login

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,7 @@ const authController = require('../controllers/auth');
 const router = require('express-promise-router')();
 
 router.post('/registration', authController.registration);
+router.post('/auth/confirm/:hash_code', authController.confirmUser);
+router.post('/auth/login', authController.login);
 
 module.exports = router;
